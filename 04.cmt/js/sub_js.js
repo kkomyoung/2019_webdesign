c1e570b9a1cb4ed081c5441a86dd692422c269c7
kya754 <jhy0088@naver.com>
2019-07-22 14:36:29 +0900

    20190722

---
+++ 04.cmt/js/sub_js.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+			$(".language > ul > li").click(function(){
+				var liNum = $(this).index();
+				console.log(liNum);
+				$(".language > ul > li").removeClass();
+				$(this).addClass("active");
+			});
+
+			$(".depth2 > ul > li").click(function(){
+				var liNum = $(this).index();
+				console.log( liNum );
+				$(".depth2 > ul > li").removeClass();
+				$(this).addClass("on")
+				$("#sub_contents>div").hide();
+				$("#sub_contents>div").eq(liNum).fadeIn();
+			});
+		});
+
+		new WOW().init();
